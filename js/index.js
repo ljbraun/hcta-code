bff09bfc61b974b4ad511354d466e64488f076f8
andrew-braun <andrew@andrewbraun.dev>
2024-06-07 14:27:26 +0400

    refactor: Move all JS functions into index because using import statements leads to CORS error on Kajabi

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
 // When embedding this, use type="module" on the scrip tag
 // e.g: <script type="module" src="script.js"></script>
 
-import { initLessonNav } from "./lesson-nav"
+// lesson-nav functions (can't use multiple files because of CORS policy)
+function scrollToCurrentLesson() {
+	const currentLessonTitle = document.querySelector(
+		".main-sidebar product-outline-post.active"
+	)
 
-// in-lesson JS
+	if (currentLessonTitle) {
+		currentLessonTitle.scrollIntoView({
+			behavior: "smooth",
+			block: "center",
+		})
+	}
+
+	return
+}
+
+export function initLessonNav() {
+	if (document.readyState === "complete") {
+		scrollToCurrentLesson()
+	} else {
+		window.addEventListener("load", scrollToCurrentLesson)
+	}
+}
+
+// init
 ;(() => {
 	console.log("JS initiated...")
 	window.addEventListener("load", initLessonNav)
